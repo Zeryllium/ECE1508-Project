36dc32f9fb21f03244ee98d62b98f1e643d1d577
Tianrui-Luo <tianrui.luo@mail.utoronto.ca>
2025-04-01 16:56:56 -0400

    New file using adv dataset to train

---
+++ ResNetWithAdvDataset.py
@@ -0,0 +1,230 @@
+import os
+from copy import deepcopy
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+import torchvision.datasets as DS
+import torchvision.transforms as transforms
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from PIL import Image
+import pandas as pd
+
+class AdversarialDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.img_dir, os.path.basename(self.data.iloc[idx, 0]))  # First column: Image Path
+        true_label = int(self.data.iloc[idx, 1])  # Second column: True Label
+        
+        image = Image.open(img_name).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, true_label
+
+
+def denormalize(image):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    transformed_image = transforms.Normalize(mean=-(mean/std), std=1/std)(image)
+    return transformed_image
+
+
+def imshow(image):
+    # remove the impact of normalization
+    denormalized_image = denormalize(image).squeeze()
+
+    plt.imshow(np.transpose(denormalized_image, (1, 2, 0)))
+    plt.show()
+
+
+def setup(params):
+    torch.manual_seed(1504)
+    np.random.seed(1504)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    batch_size = params.get("batch_size")
+
+    train_dataset = AdversarialDataset(
+        csv_file="./adversarial_dataset/train/train_adv_samples.csv", 
+        img_dir="./adversarial_dataset/train", 
+        transform=transform
+    )
+    val_dataset = AdversarialDataset(
+        csv_file="./adversarial_dataset/validation/valid_adv_samples.csv", 
+        img_dir="./adversarial_dataset/validation", 
+        transform=transform
+    )
+    test_dataset = AdversarialDataset(
+        csv_file="./adversarial_dataset/test/test_adv_samples.csv", 
+        img_dir="./adversarial_dataset/test", 
+        transform=transform
+    )
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+
+    # resnet18 has a final FC layer with 1000 output features, corresponding to the 1000 image classes in ImageNet
+    # replace this with 10 output features
+    #model = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=False)
+    model = torchvision.models.resnet18(weights=None)
+    model.fc = torch.nn.Linear(in_features=512, out_features=10, bias=True)
+
+    if torch.backends.cuda.is_built():
+        # if we have cuda
+        # usually on Windows machines with GPU
+        device = "cuda"
+    elif torch.backends.mps.is_built():
+        # if we have MPS
+        # usually on MAC
+        device = "mps"
+    else:
+        # if not we should use our CPU
+        device = "cpu"
+
+    return train_loader, val_loader, test_loader, model, device
+
+
+def test_model(model, test_loader, device):
+    model.eval()
+
+    test_accuracy = 0
+    for batch_images, batch_labels in test_loader:
+        batch_images = batch_images.to(device)
+        batch_labels = batch_labels.to(device)
+
+        batch_predictions = model.forward(batch_images)
+
+        batch_accuracy = torch.count_nonzero(torch.eq(
+            torch.argmax(torch.nn.functional.softmax(batch_predictions, dim=1), dim=1),
+            batch_labels
+        ))
+
+        test_accuracy += batch_accuracy.item()
+
+    test_accuracy = test_accuracy / len(test_loader.dataset)
+
+    return test_accuracy
+
+
+def run(params):
+    print(f"Running with parameters: {params}")
+    train_loader, val_loader, test_loader, model, device = setup(params)
+    print(f"Device: {device}")
+
+    model.to(device)
+    optimizer = params.get("optimizer")(model.parameters(), lr=params.get("lr"))
+    loss_fn = params.get("loss_fn")
+
+    epoch_train_loss = []
+    epoch_train_acc = []
+    epoch_val_loss = []
+    epoch_val_acc = []
+
+    epoch_best = ()
+
+    for epoch in range(params.get("epochs")):
+        model.train()
+        train_loss = 0
+        train_accuracy = 0
+
+        for batch_images, batch_labels in train_loader:
+            batch_images = batch_images.to(device)
+            batch_labels = batch_labels.to(device)
+
+            batch_predictions = model.forward(batch_images)
+            batch_loss = loss_fn(batch_predictions, batch_labels)
+            batch_accuracy = torch.count_nonzero(torch.eq(
+                torch.argmax(torch.nn.functional.softmax(batch_predictions, dim=1), dim=1),
+                batch_labels
+            ))
+
+            train_loss += batch_loss.item()
+            train_accuracy += batch_accuracy.item()
+
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+        epoch_train_loss.append(train_loss/len(train_loader.dataset))
+        epoch_train_acc.append(train_accuracy/len(train_loader.dataset))
+
+        model.eval()
+        val_loss = 0
+        val_accuracy = 0
+
+        for batch_images, batch_labels in val_loader:
+            batch_images = batch_images.to(device)
+            batch_labels = batch_labels.to(device)
+
+            batch_predictions = model.forward(batch_images)
+            batch_loss = loss_fn(batch_predictions, batch_labels)
+            batch_accuracy = torch.count_nonzero(torch.eq(
+                torch.argmax(torch.nn.functional.softmax(batch_predictions, dim=1), dim=1),
+                batch_labels
+            ))
+
+            val_loss += batch_loss.item()
+            val_accuracy += batch_accuracy.item()
+        epoch_val_loss.append(val_loss / len(val_loader.dataset))
+        epoch_val_acc.append(val_accuracy / len(val_loader.dataset))
+
+        print(f"Epoch: {epoch} | Train Loss: {epoch_train_loss[-1]:.4f} | Train Acc: {epoch_train_acc[-1]:.4f} || Val Loss: {epoch_val_loss[-1]:.4f} | Val Acc: {epoch_val_acc[-1]:.4f}")
+
+        if np.argmin(epoch_val_loss) == epoch:
+            epoch_best = (deepcopy(model.state_dict()), epoch)
+
+
+    # plot the training and test losses
+    plt.plot([i for i in range(params.get("epochs"))], epoch_train_loss, label='Training')
+    plt.plot([i for i in range(params.get("epochs"))], epoch_val_loss, label='Validation')
+    plt.legend()
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show()
+
+    # plot the test accuracy
+    plt.plot([i+1 for i in range(params.get("epochs"))], epoch_train_acc, label='Training')
+    plt.plot([i+1 for i in range(params.get("epochs"))], epoch_val_acc, label='Validation')
+    plt.legend()
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+    print(f"Saving epoch best: {epoch_best[1]}")
+    save_path = params.get("model_save_path").format(model_name=params.get("model_name"))
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+
+    torch.save(epoch_best[0], os.path.join(save_path, f"epoch_{epoch_best[1]}.pth"))
+
+    test_accuracy = test_model(model, test_loader, device)
+    print(f"Model test accuracy: {test_accuracy}")
+
+if __name__ == "__main__":
+    # TODO: set up command-line argparse
+    params ={
+        "batch_size": 64,
+        "epochs": 20,
+        "loss_fn": torch.nn.CrossEntropyLoss(),
+        "optimizer": torch.optim.Adam,
+        "lr": 1e-3,
+        "model_name": "resnet18_base",
+        "model_save_path": "./models/{model_name}/",
+    }
+
+    run(params)
